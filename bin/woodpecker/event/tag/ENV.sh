a95a75c1359bb5d03b0c0b785a5edb5e548dff62
luli <riku.ri@outlook.com>
2025-12-29 02:40:07 +0800

    Less log from ENV.sh

--- bin/woodpecker/event/tag/ENV.sh
+++ bin/woodpecker/event/tag/ENV.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex
+set -euo pipefail
 # woodpecker tag event variables:
 CI_COMMIT_TAG="${CI_COMMIT_TAG}" && test -n "${CI_COMMIT_TAG}"
 CI_REPO_URL="${CI_REPO_URL}" && test -n "${CI_REPO_URL}"
@@ -11,4 +11,3 @@ CI_REPO_CLONE_URL="${CI_REPO_CLONE_URL}" && test -n "${CI_REPO_CLONE_URL}"
 # other variables:
 API_URL="${API_URL}" && test -n "${API_URL}"
 	#https://codeberg.org/api/v1
-set +x
