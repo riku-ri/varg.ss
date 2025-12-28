8acbd4585b59f37dbb13b6074b8e65552e0b163d
luli <riku.ri@outlook.com>
2025-12-29 01:35:54 +0800

    Use sed but not xargs in release-info action

--- bin/woodpecker/event/tag/release-info.sh
+++ bin/woodpecker/event/tag/release-info.sh
@@ -11,15 +11,16 @@ curl -sSL \
 	-X 'GET' \
 	-H 'accept: application/json' \
 	"$API_URL/repos/$CI_REPO/releases" \
-| jq '.[].tag_name' | xargs -I__ -d"\n" echo '(release __)' | tee -a $CI_REPO_NAME.release-info
+| jq '.[].tag_name' -c | sed 's/^/(release /' | sed 's/$/)/' \
+| tee -a $CI_REPO_NAME.release-info
 
-id="$(curl -sSL \
-	-X 'GET' \
-	-H 'accept: application/json' \
-	"$API_URL/repos/$CI_REPO/releases/tags/$CI_COMMIT_TAG" \
-	| jq '.id')"
-
-curl -u :$TOKEN -X 'POST' \
-	-H 'accept: application/json' \
-	--data-binary "@$CI_REPO_NAME.release-info" \
-	"$API_URL/repos/$CI_REPO/releases/$id/assets?name=$CI_REPO_NAME.release-info"
+#id="$(curl -sSL \
+#	-X 'GET' \
+#	-H 'accept: application/json' \
+#	"$API_URL/repos/$CI_REPO/releases/tags/$CI_COMMIT_TAG" \
+#	| jq '.id')"
+#
+#curl -u :$TOKEN -X 'POST' \
+#	-H 'accept: application/json' \
+#	--data-binary "@$CI_REPO_NAME.release-info" \
+#	"$API_URL/repos/$CI_REPO/releases/$id/assets?name=$CI_REPO_NAME.release-info"
