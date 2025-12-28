40f3c856734a9274c0b63d441fc4f97add1805b6
luli <riku.ri@outlook.com>
2025-12-29 01:39:39 +0800

    Use bash but not sh in CI, always pull image

--- bin/woodpecker/event/tag/release-info.sh
+++ bin/woodpecker/event/tag/release-info.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -eo pipefail
 
@@ -14,13 +14,13 @@ curl -sSL \
 | jq '.[].tag_name' -c | sed 's/^/(release /' | sed 's/$/)/' \
 | tee -a $CI_REPO_NAME.release-info
 
-#id="$(curl -sSL \
-#	-X 'GET' \
-#	-H 'accept: application/json' \
-#	"$API_URL/repos/$CI_REPO/releases/tags/$CI_COMMIT_TAG" \
-#	| jq '.id')"
-#
-#curl -u :$TOKEN -X 'POST' \
-#	-H 'accept: application/json' \
-#	--data-binary "@$CI_REPO_NAME.release-info" \
-#	"$API_URL/repos/$CI_REPO/releases/$id/assets?name=$CI_REPO_NAME.release-info"
+id="$(curl -sSL \
+	-X 'GET' \
+	-H 'accept: application/json' \
+	"$API_URL/repos/$CI_REPO/releases/tags/$CI_COMMIT_TAG" \
+	| jq '.id')"
+
+curl -u :$TOKEN -X 'POST' \
+	-H 'accept: application/json' \
+	--data-binary "@$CI_REPO_NAME.release-info" \
+	"$API_URL/repos/$CI_REPO/releases/$id/assets?name=$CI_REPO_NAME.release-info"
