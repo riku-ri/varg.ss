b6576dc9889bc837e83ab0a1937cba889b705f34
luli <riku.ri@outlook.com>
2025-12-29 01:21:13 +0800

    Dynamic tag name in release.sh

--- bin/woodpecker/event/tag/release.sh
+++ bin/woodpecker/event/tag/release.sh
@@ -13,5 +13,5 @@ curl -sSL \
 	-X 'POST' \
 	-H 'accept: application/json' \
 	-H 'Content-Type: application/json' \
-	-d '{''"body": '"\"$body\" , "'"name": "0.0.0" ,  "tag_name": "0.0.0"''}' \
+	-d '{''"body": '"\"$body\" , "'"name": ''"'"$CI_COMMIT_TAG"'"'' ,  "tag_name": ''"'$CI_COMMIT_TAG'"}' \
 	"$API_URL/repos/$CI_REPO/releases"
